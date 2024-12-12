ff5701406a0163503d25d2a4bc2614d894bbc5c3
Elena Vedeneva <astroshelest@gmail.com>
2024-12-12 17:17:16 +0100

    Added a function for creating dispatcher

---
+++ commands/create_dispatcher.py
@@ -0,0 +1,11 @@
+from components import Dispatcher
+
+
+def create_dispatcher() -> Dispatcher:
+    """
+    Utility function.
+
+    - create_dispatcher() -> Dispatcher:
+        Instantiates and returns a new `Dispatcher` object.
+    """
+    return Dispatcher()
